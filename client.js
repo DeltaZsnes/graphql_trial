52e55feb3f23827984c11eb6aa8f74c40393ccdc
Daniel Zheng <daniel.zheng@barnebys.com>
2019-09-05 13:06:31 +0200

    add courses

--- client.js
+++ client.js
@@ -8,8 +8,15 @@ const helloWorld = async() => {
 	console.log(json);
 }
 
+const courses = async () => {
+	const response = await fetch("http://localhost:4000/graphql?", {"credentials":"include","headers":{"accept":"application/json","accept-language":"en,sv;q=0.9,ja;q=0.8","cache-control":"no-cache","content-type":"application/json","pragma":"no-cache","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"},"referrer":"http://localhost:4000/","referrerPolicy":"origin","body":"{\"query\":\"{\\n  courses {\\n    id\\n  }\\n}\",\"variables\":null}","method":"POST","mode":"cors"});
+	const json = await response.json();
+	console.log(json)
+}
+
 const runAsync = async () => {
 	await helloWorld();
+	await courses();
 };
 
 runAsync();
